04d6f11728d1467b6ac5707ad5eb7f8d1d7f0912
D34DSH0T-77 <cjoyner1402@gmail.com>
2025-11-22 10:55:06 -0400

    BD en Red

--- Config/config.php
+++ Config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('RUTA_BASE', 'http://localhost/SFT/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'tortas');
+define('DB_HOST', 'mysql-deadshot.alwaysdata.net');
+define('DB_USER', 'deadshot');
+define('DB_PASS', '__Hola1234__');
+define('DB_NAME', 'deadshot_tortas');
 define('UTF8', 'utf8mb4');

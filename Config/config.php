f96ade97ced76076cc0b76f37e6fabe4bf06188e
D34DSH0T-77 <cjoyner1402@gmail.com>
2026-01-12 16:39:07 -0400

    DB Listo

--- Config/config.php
+++ Config/config.php
@@ -5,4 +5,5 @@ define('DB_HOST', 'mysql-deadshot-77-cjoyner1402-941e.g.aivencloud.com');
 define('DB_USER', 'avnadmin');
 define('DB_PASS', 'AVNS_k0D88rBytH1YDz-awCR');
 define('DB_NAME', 'defaultdb');
+define('DB_PORT', '27909');
 define('UTF8', 'utf8mb4');
--- src/Conex/Conexion.php
+++ src/Conex/Conexion.php
@@ -11,7 +11,7 @@ class Conexion {
 
     public function __construct() {
         try {
-            $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . UTF8, DB_USER, DB_PASS);
+            $this->conn = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=" . UTF8, DB_USER, DB_PASS);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Conexion fallida :V " . $e->getMessage());
